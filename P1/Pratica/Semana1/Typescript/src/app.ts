64108d9e2dfe05dba9dcff99344fc06f7e95104e
kristhian14 <kristhianbello12@gmail.com>
2025-05-13 16:44:46 -0500

    semana 3

--- P1/Pratica/Semana1/Typescript/src/app.ts
+++ P1/Pratica/Semana1/Typescript/src/app.ts
@@ -1,84 +1,29 @@
-import { createTypeReferenceDirectiveResolutionCache, escapeLeadingUnderscores } from "typescript";
+import { ConnectionIsNotSetError } from "typeorm";
+import {ConsulAll, consult, insertaUser, update, userdelete, viewdelete} from "./crud"
+import {iniciar} from "./database"
+import "reflect-metadata";
+import { CrearVista } from './crud'
+import { obtenerUser } from './crud'
+import { isAwaitKeyword } from "typescript";
 
-let saludo:string = "hello world"
-console.log(saludo)
 
-const student: Istudent={
-    id:1102,
-    nombre:'Fab',
-    correo:'fabianaa@gmail.com',
-    direccion:'su casa',
+async function main() {
+    await iniciar()
+    const newUser= await insertaUser("walter", "walterG@gmail.com")
+    console.log ("usuario creado", newUser.id)
+
+    const vista = await CrearVista("vista Reporte de Productos", newUser.id);
+    console.log(vista);
+
+    const vistadelet = await viewdelete(vista!.id)
+    console.log(vistadelet)
+    const users = await ConsulAll();
+    console.log (users)
+    const userOne = await consult(newUser.id)
+    console.log(userOne)
+    const userUpdated= await update(newUser.id, "fabiana", "valentina");
+    console.log(userUpdated)
+    const userDeleted = await userdelete(newUser.id);
+    console.log(userDeleted)
 }
-
-interface Istudent {
-    id:number;
-    nombre:string;
-    correo:string;
-    direccion:string;
-    calificacion?:number; //el ? significa que calificaciones esta en el sistema pero no es "obligatorio"
-}
-
-const students:Istudent[]=[
-    {
-        id:1102,
-        nombre:'fab',
-        correo: 'fabianaa@gmail.com',
-        direccion:'su casa',
-    }
-]
-students.push({id:1102, nombre:"fab", correo:"fabianaa@gmail.com", direccion:"en mi casa"});
-
-students.push(student)
-
-function agregar(student: Istudent):void{
-    students.push(student);
-}
-
-const estudiante1:Istudent={id:2, nombre:'', correo:'', direccion:''}
-agregar(student)
-
-function agregar2(parm:Istudent, callback:(student:Istudent)=>void)
-{
-    students.push(parm)
-    callback(parm)
-}
-
-const estudianteA2: Istudent = {id:2, nombre:'', correo:'', direccion:''}
-
-agregar2(estudianteA2, (parm:Istudent)=>console.log);
-
-function agregar3(parm:Istudent):Promise<Istudent>
-{
-    return new Promise((resolve)=>{
-        students.push(parm);
-        setTimeout(()=>{
-            resolve(parm)
-
-        },
-        1000
-        )
-    },
-    )
-    
-}
-
-agregar3(estudiante1).then((Istudent)=>
-{
-    console.log(student);
-})
-
-
-/*async function main() {
-    try
-    {
-        await agregar3(estudiante1)
-    }
-    catch(ex){
-
-    }
-    finally
-    {
-
-    }
-}
-main()*/
+main()
